be19267ef23968381805c49124a1f37c7f64d602
zobayerhb <hosenzobayer55@gmail.com>
2025-02-26 00:09:18 +0600

    cart count show with specific email

--- index.js
+++ index.js
@@ -44,7 +44,9 @@ async function run() {
     // carts collection
     // add to cart data POST api create
     app.get("/carts", async (req, res) => {
-      const result = await cartsCollection.find().toArray();
+      const email = req.query.email;
+      const query = { email: email };
+      const result = await cartsCollection.find(query).toArray();
       res.send(result);
     });
     app.post("/carts", async (req, res) => {

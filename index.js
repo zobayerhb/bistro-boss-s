63737f0760f1cb844e5fd0987a7b4da5c25a8dc7
zobayerhb <hosenzobayer55@gmail.com>
2025-02-28 22:22:49 +0600

    delete user by admin

--- index.js
+++ index.js
@@ -66,8 +66,14 @@ async function run() {
       const result = await usersCollection.insertOne(user);
       res.send(result);
     });
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const result = await usersCollection.deleteOne(filter);
+      res.send(result);
+    });
 
-    // carts collection
+    // ========== carts collection ============
     // add to cart data POST api create
     app.get("/carts", async (req, res) => {
       const email = req.query.email;

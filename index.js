4618255151d15b433ce9f816644d8d0f6f02d25f
zobayerhb <hosenzobayer55@gmail.com>
2025-02-28 22:46:39 +0600

    create admin create api

--- index.js
+++ index.js
@@ -66,6 +66,20 @@ async function run() {
       const result = await usersCollection.insertOne(user);
       res.send(result);
     });
+
+    // =========== MAKE ADMIN API ===========
+    app.patch("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: "admin",
+        },
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
     app.delete("/users/:id", async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };

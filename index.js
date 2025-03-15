ee6b4aa14ba505487a073742a7b9fde6dae13b7a
zobayerhb <hosenzobayer55@gmail.com>
2025-03-15 21:58:54 +0600

    payment intent create

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
@@ -219,6 +220,21 @@ async function run() {
       const filter = { _id: new ObjectId(id) };
       const result = await cartsCollection.deleteOne(filter);
       res.send(result);
+    });
+
+    // ============ STRIPE PAYMENT METHOD INTREGATE ==============
+    app.post("create-payment-intent", async (req, res) => {
+      const { item } = req.body;
+      const amount = parseInt(item * 100);
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount,
+        currency: "usd",
+        payment_method_types: ["card"],
+      });
+
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      });
     });
 
     // Send a ping to confirm a successful connection
